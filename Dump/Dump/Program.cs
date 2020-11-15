fecf41c60c20f84c1a8aac8be4d81c017f7c0ac6
lklein2505 <lklein00@fesb.hr>
2020-11-15 19:07:25 +0100

    Added console writeline for second commit

--- Dump/Dump/Program.cs
+++ Dump/Dump/Program.cs
@@ -7,6 +7,7 @@ namespace Dump
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello from second commit!");
         }
     }
 }
